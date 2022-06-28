d2491127f9c0ee413c6d176f283604e2c4bb6c22
schillerandrew <schiller.andrew@gmail.com>
2022-06-27 20:37:10 -0700

    updated events.js and now successfully logging pickup event and delivery messages

--- events.js
+++ events.js
@@ -1,14 +1,42 @@
 'use strict';
 
 const eventPool = require('./eventPool');
+const Chance = require('chance');
 const driverHandler = require('./driver/handleDriver');
 const vendorHandler = require('./vendor/handleVendor');
+
+const chance = new Chance();
+
+// let randomPayload = {
+//   store: chance.company(),
+//   orderID: chance.fbid(),
+//   customer: chance.name(),
+//   address: chance.address()
+// }
+
+// chance.name(); full name
+// chance.address(); street address
+// chance.fbid() id
+// chance.company() store name
+
+const dateAndTime = new Date();
+// console.log(dateAndTime);
 
 eventPool.on('PICKUP', driverHandler);
 // eventPool.on('IN-TRANSIT');
 // eventPool.on('DELIVERED');
 
-setInterval( () => {
+setInterval(() => {
+  let event = {
+    event: 'pickup',
+    time: dateAndTime,
+    payload: {
+      store: chance.company(),
+      orderID: chance.fbid(),
+      customer: chance.name(),
+      address: chance.address()
+    }
+  };
+  console.log('EVENT', event);
   eventPool.emit('PICKUP', { test: 'test' });
-  console.log(eventPool);
 }, 1000);
